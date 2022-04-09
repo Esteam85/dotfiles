496e529a78e58ea6f5df3eded499a7a342f89586
Esteban Contreras <esteban.contreras@walmart.com>
2022-04-09 11:55:14 -0400

    feat: Addings files for HomeBrew, vscode, git, zsh, Rocket and symlinks
    Refs: EC-1

---
+++ symlinks/links.sh
@@ -0,0 +1,4 @@
+ln -s $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
+ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/mac/Rocket/rocket.db $HOME/Library/Application\ Support/Rocket/rocket.db
+ln -s $HOME/.dotfiles/ide/vscode/settings.json $HOME/Library/Application\ Support/Code/Usersettings.json

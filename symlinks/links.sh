ba37d2aa6ac6af5b4abdf6076457a1c46f73cea4
Esteban Contreras <contacto@econtreras.net>
2023-05-04 22:55:20 -0400

    refactor: removing nvm

--- symlinks/links.sh
+++ symlinks/links.sh
@@ -13,7 +13,6 @@ ln -s $HOME/.dotfiles/shell/zsh/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/shell/zsh/.zshenv $HOME/.zshenv
 ln -s $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
 
-ln -s $HOME/.dotfiles/mac/.nvm $HOME/.nvm
 ln -s $HOME/.dotfiles/mac/iterm2/.hushlogin $HOME/.hushlogin
 ln -s $HOME/.dotfiles/mac/Rocket/rocket.db $HOME/Library/Application\ Support/Rocket/rocket.db
 ln -s $HOME/.dotfiles/ides/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json

bdcf1218afdc9bfaaf821ad877316f7b8b09502e
Esteban Contreras <contacto@econtreras.net>
2023-05-12 09:19:17 -0400

    feat: adding i. command

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -2,6 +2,7 @@
 alias c.='(code $PWD &>/dev/null &)'
 alias w.='(webstorm $PWD &>/dev/null &)'
 alias g.='(goland $PWD &>/dev/null &)'
+alias i.='(idea $PWD &>/dev/null &)'
 
 #apps
 alias cat='ccat'

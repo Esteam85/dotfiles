fd8da867baa0a61ba1eaea1506376cd53d066457
Esteban Contreras <contacto@econtreras.net>
2023-05-15 12:30:51 -0400

    feat(symlinks): adding sl to icloud folder

--- symlinks/links.sh
+++ symlinks/links.sh
@@ -16,3 +16,4 @@ ln -s $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
 ln -s $HOME/.dotfiles/mac/iterm2/.hushlogin $HOME/.hushlogin
 ln -s $HOME/.dotfiles/mac/Rocket/rocket.db $HOME/Library/Application\ Support/Rocket/rocket.db
 ln -s $HOME/.dotfiles/ides/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+ln -s $HOME/Library/Mobile\ Documents/com~apple~CloudDocs/ $HOME/icloud

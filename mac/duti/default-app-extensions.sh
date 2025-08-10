820f975717762da75b499aea9af5f05eae96dc82
Esteban Enrique Contreras Tapia <contacto@econtreras.net>
2025-08-10 18:40:41 -0400

    feat: adding more app extension

--- mac/duti/default-app-extensions.sh
+++ mac/duti/default-app-extensions.sh
@@ -3,3 +3,8 @@ duti -s com.microsoft.VSCode yaml all
 duti -s com.microsoft.VSCode txt all
 duti -s com.microsoft.VSCode csv all
 duti -s com.microsoft.VSCode md all
+duti -s com.microsoft.VSCode json all
+duti -s com.microsoft.VSCode py all
+duti -s com.microsoft.VSCode srt all
+duti -s com.microsoft.VSCode properties all
+duti -s com.microsoft.VSCode dmn all
